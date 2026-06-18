4cba778a513343fe9488b69f68eb967f48058cb5
Yuvraj Singh Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-06-18 11:09:13 +0530

    Implement agreement prompt in agree.c

---
+++ agree.c
@@ -0,0 +1,9 @@
+#include <cs50.h>
+#include <stdio.h>
+int main(void){
+    char c = get_char("Do you agree? \n");
+    if (c == 'y' || c == 'Y')
+    {printf ("Agreed\n");}
+        else {printf ("Not agreed\n");}
+        return 0;
+}
