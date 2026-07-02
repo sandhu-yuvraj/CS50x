300931815e1b98707a4b9a1fe3d2fc26a9d50c98
Yuvraj Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-07-02 11:14:02 +0530

    Update

--- compare.c
+++ compare.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(void){
     int x , y;
-    printf("What's x? \n");
+    printf("What's x? ");
     scanf("%d" , &x);
-    printf("What's y? \n");
+    printf("What's y? ");
     scanf("%d" , &y);
     if (x<y)
     {printf("x is less than y\n");}
