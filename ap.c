da664d03646cf41d5f60bbf0f8c0133285c09129
Yuvraj Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-07-02 22:43:52 +0530

    Update to include sum and nth term

--- ap.c
+++ ap.c
@@ -9,9 +9,15 @@ int main(){
     int terms;
     printf("Enter number of terms: ");
     scanf("%d" , &terms);
-    for ( int i =1 ; i <= terms ; i+=diff)
+    int nth = firstTerm + (terms-1)*diff ;
+    int sum = 0;
+    for ( int i =firstTerm ; i <= nth ; i+=diff)
     {
         printf("%d " , i);
+        sum += i;
     }
+    printf("\n");
+     printf("Sum of terms of AP: %d\n" , sum);
+     printf("nth term is: %d\n" , nth);
        return 0;
 }
